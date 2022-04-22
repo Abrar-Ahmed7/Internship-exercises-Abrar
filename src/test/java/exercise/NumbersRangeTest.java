4defb9146a2569c5380c29d9218b4af44503fbb4
Ahmed <abrarahmedid@gmail.com>
2022-04-22 10:13:24 +0530

    bakery problem

--- src/test/java/exercise/NumbersRangeTest.java
+++ src/test/java/exercise/NumbersRangeTest.java
@@ -1,12 +1,15 @@
 package exercise;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 
 public class NumbersRangeTest {
@@ -15,8 +18,13 @@ public class NumbersRangeTest {
 
     @Test
     public void itShouldClassifyNumbersBetweenTheRange(){
-        List<String> resList = numbersRange.inBetween(56776522,56776537);
-        List<String> testList = new ArrayList<>(Arrays.asList("even", "56776523", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd"));
-        assertThat(resList,is(testList));
+//        List<String> resList = numbersRange.inBetween(56776522,56776537);
+//        Stream<String> testStream = Stream.of("odd","2","odd","even","5","even","7","even","odd","even");
+//        List<String> testList = new ArrayList<>(Arrays.asList("even", "56776523", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd"));
+//        List<String> testList = new ArrayList<>(Arrays.asList("odd","2","odd","even","5","even","7","even","odd","even"));
+//        assertThat(resStream.toArray(),is(testList));
+        String[] testStringArray = {"odd", "2", "3", "even", "5", "even", "7", "even", "odd", "even"};
+        Stream<String> resStream= numbersRange.inBetween(1,10);
+        Assert.assertArrayEquals(testStringArray,resStream.toArray());
     }
 }
--- src/test/java/exercise/grep/GrepFunctionalityTest.java
+++ src/test/java/exercise/grep/GrepFunctionalityTest.java
@@ -13,28 +13,34 @@ public class GrepFunctionalityTest {
 
     @Test
     public void itShouldReturnMatchedLines() {
+//        Given
         String expectedRes = "and like other forms of punctuation they are meant to make written material easy to read.”\n" +
                 "you simply cannot clearly convey sequential points and their relationships to one another.";
+//        When
         String res = grepFunctionality.readFromFile(Path.of("src\\test\\resources\\sample.txt"),"to");
+//        Then
         Assert.assertThat(res, is(expectedRes));
     }
 
     @Test
     public void itShouldWriteToFile() {
+//        Given
+        String expectedRes = "paragraphs are “in essence—a form of punctuation,\n" +
+            "and like other forms of punctuation they are meant to make written material easy to read.”\n" +
+            "Effective paragraphs are the fundamental units of academic writing;";
+//        When
         grepFunctionality.readAndWrite(Path.of("src\\test\\resources\\sample.txt"),
                 Path.of("src\\test\\resources\\output.txt"),
                 "aRE");
-        String expectedRes = "paragraphs are “in essence—a form of punctuation,\n" +
-                "and like other forms of punctuation they are meant to make written material easy to read.”\n" +
-                "Effective paragraphs are the fundamental units of academic writing;";
         String res = grepFunctionality.readFromFile(Path.of("src\\test\\resources\\output.txt"), "aRE");
+//        Then
         Assert.assertThat(res, is(expectedRes));
     }
 
     @Test
     public void itShouldPrintTheMatchedLinesFromAllRegularFiles() {
 //        D:\Internship-tasks\www\w
-        grepFunctionality.recursiveSearch(Path.of("D:\\Internship-tasks\\www\\w"),"hi");
+        grepFunctionality.recursiveSearch(Path.of("src\\resources"),"tHe");
     }
 
 }
